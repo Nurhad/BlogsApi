5528049673fddf61921dcc4ed6de99528ae9fbde
Nurhad <m.nurhadbarokah@gmail.com>
2021-04-30 17:22:35 +0700

    Add project files.

---
+++ BlogsApi/Entities/BlogsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogsApi.Entities
+{
+    public class BlogsDbContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public BlogsDbContext(DbContextOptions<BlogsDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().ToTable("Blog");
+            modelBuilder.Entity<Category>().ToTable("Category");
+            modelBuilder.Entity<Blog>().HasMany(s => s.Categories).WithMany(c => c.Blogs);
+        }
+    }
+}
---
+++ BlogsApi/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using BlogsApi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogsApi.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetCategories();
+    }
+}
